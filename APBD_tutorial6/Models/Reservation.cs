cc7722ad62824bbf66cbafd4f22dcf71d8fc8c4b
De-Gin <kuznetsovilas@gmail.com>
2026-04-20 20:18:54 +0200

    Added models

---
+++ APBD_tutorial6/Models/Reservation.cs
@@ -0,0 +1,17 @@
+namespace APBD_tutorial6.Models;
+
+public class Reservation
+{
+    public  int Id;
+    public  int RoomId;
+    public string OrganizerName;
+    public string Description;
+    public  string Topic;
+    public  DateOnly Date;
+    public  TimeOnly StartTime;
+    public  TimeOnly EndTime;
+    public  string Status;
+
+
+
+}
---
+++ APBD_tutorial6/Models/Room.cs
@@ -0,0 +1,14 @@
+namespace APBD_tutorial6.Models;
+
+public class Room
+{
+    public int Id;
+    public string Name;
+    public string BuildingCode;
+    public int Floor;
+    public int Capacity;
+    public bool HasProjector;
+    public bool IsActive;
+
+
+}
